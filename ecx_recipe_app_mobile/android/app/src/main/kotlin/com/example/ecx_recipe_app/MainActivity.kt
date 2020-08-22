5a20c55f0d6090a66738972009c9bbad4bbdc774
lordlamee <gabrielolamideakorede@gmail.com>
2020-08-23 00:49:23 +0100

    initial mobile commit
    mobile starting file

---
+++ ecx_recipe_app_mobile/android/app/src/main/kotlin/com/example/ecx_recipe_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.ecx_recipe_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ ecx_recipe_app_mobile/lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(EcxRecipeApp());
+  }
+  
+  class EcxRecipeApp extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+
+    );
+  }
+}
+
